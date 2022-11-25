3e530ef3bb80eccd70abef2c35402d0a35d0e19b
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2022-11-25 10:30:01 +0100

    menu sidebar for mobile implemented and minor changes to home pages

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,18 +1,33 @@
 import React from 'react'
+import { useState } from 'react';
 // import { AiOutlineHeart } from 'react-icons/ai';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import '../styles/Nav.css';
 
 function Nav() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+  function toggle () {
+    isOpen === true ? setIsOpen(false) : setIsOpen(true)
+  }
+
   return (
     <nav>
       <section className='mobile'>
+
         <section className="section row">
-          <section className="section column menu">
-            <select name="categories">
-              <option></option>
-            </select>
-          </section>
+          <aside className="section column menu">
+            <div id="mySidepanel" className="sidepanel" style={ {width: isOpen ? '300px' : '0px'} }>
+              <a href="javascript:void(0)" className="closebtn" onClick={toggle}>×</a>
+              <a className="a column" href='http://localhost:3000/living-room'>Living Room</a>
+              <a className="a column" href='http://localhost:3000/kitchen'>Kitchen</a>
+              <a className="a column" href='http://localhost:3000/bedroom'>Bedroom</a>
+              <a className="a column" href='http://localhost:3000/bathroom'>Bathroom</a>
+              <a className="a column" href='http://localhost:3000/common-areas'>Common Areas</a>
+            </div>
+            <button className="openbtn" onClick={toggle}>☰</button>
+          </aside>
           <section className="section column logo">
             <h1>CIKC</h1>
           </section>
@@ -38,11 +53,13 @@ function Nav() {
         </section> */}
 
         <section className="section row">
+
           <section className="section column logo">
             <h1>CIKC</h1>
           </section>
+
           <section className="section column searchbox">
-            <input type="text" placeholder="Search.."></input>ç
+            <input type="text" placeholder="Search.."></input>
           </section>
           <section className="section column iconAndButton">
             <a className="a shoppingBagIcon" id="linkOne" href="#thingOne"><HiOutlineShoppingBag /></a>
@@ -57,8 +74,9 @@ function Nav() {
           <a className="a column" href='http://localhost:3000/bathroom'>Bathroom</a>
           <a className="a column" href='http://localhost:3000/common-areas'>Common Areas</a>
         </section>
-
       </section>
+
+      <h2 className="h1">Black Friday Offers end in 9 days!</h2>
     </nav>
   )
 }
